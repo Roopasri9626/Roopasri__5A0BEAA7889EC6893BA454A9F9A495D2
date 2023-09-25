4301e4db22386413a06f5bbfd099b973c649959f
Roopasri9626 <145997618+Roopasri9626@users.noreply.github.com>
2023-09-25 19:26:01 +0530

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,44 @@
+# 2.1 Implement a class called BankAccount that represents a bank account. The class should have private attributes for account number, account holder name, and account balance. Include methods to deposit money, withdraw money, and display the account balance. Ensure that the account balance cannot be accessed directly from outside the class. Write a program to create an instance of the BankAccount class and test the dep
+#2.2 Implement a class called Player that represents a cricket player. The Player class should have a method called play() which prints "The player is playing cricket. Derive two classes, Batsman and Bowler, from the Player class. Override the play() method in each derived class to print "The batsman is batting" and "The bowler is bowling", respectively. Write a program to create objects of both the Batsman and Bowler classes and call the play() method for each object.
+
+# Define the Player class
+
+
+class Player:
+
+  def play(self):
+
+    print("The player is playing cricket.")
+
+
+# Define the Batsman class, derived from Player
+
+
+class Batsman(Player):
+
+  def play(self):
+
+    print("The batsman is batting.")
+
+
+# Define the Bowler class, derived from Player
+
+
+class Bowler(Player):
+
+  def play(self):
+
+    print("The bowler is bowling.")
+
+
+# Create objects of Batsman and Bowler classes
+
+batsman = Batsman()
+
+bowler = Bowler()
+
+# Call the play() method for each object
+
+batsman.play()
+
+bowler.play()
